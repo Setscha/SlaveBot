4b40a0b2ebbd725b5c16a4195080770b74063e0d
Setscha <tobias.seczer@gmail.com>
2021-02-05 15:16:29 +0100

    Add avatar

--- slave-bot.js
+++ slave-bot.js
@@ -4,7 +4,7 @@ const { prefix, token } = require('./config.json');
 
 const serverID = '489472550573703169';
 const roleID = '692387593635233832';
-const userIDs = ['459360514901016588']
+const userIDs = ['459360514901016588', '243081214804623362']
 
 let guild;
 let role;
@@ -13,6 +13,10 @@ client.once('ready', () => {
     console.log('Ready!');
     guild = client.guilds.cache.find(g => g.id === serverID);
     role = guild.roles.cache.find(role => role.id === roleID);
+    if (!client.user.avatar) {
+        client.user.setAvatar('./avatar.png');
+    }
+    client.user.setPresence({ activity: { name: 'Coja slave'}, status: 'online' })
 });
 
 client.login(token);
@@ -26,16 +30,20 @@ client.on('message', message => {
 
     if (command === 'slave') {
         userIDs.forEach(id => {
-            const user = guild.members.cache.find(m => m.id === id);
-            user.roles.add(role);
+            const user = guild.members.cache.find(m => m.id == id);
+            if (user) {
+                user.roles.add(role);
+            }
         });
         message.channel.send('Coja slave');
 	} else if(command === 'noslave') {
         if(userIDs.includes(message.author.id)) return;
 
         userIDs.forEach(id => {
-            const user = guild.members.cache.find(m => m.id === id);
-            user.roles.remove(role);
+            const user = guild.members.cache.find(m => m.id == id);
+            if (user) {
+                user.roles.remove(role);
+            }
         });
         message.channel.send('Coja no slave :c');
     }
